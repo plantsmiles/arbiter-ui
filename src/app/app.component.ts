dc3f59e5242bf53b6c7250e1949970362ee56d09
plantsmiles <plantsmiles@gmail.com>
2017-10-08 10:23:53 -0600

    both exchanges on d3 chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,73 @@ import { OrderBookService } from './providers/order.book.service';
 })
 export class AppComponent implements OnInit {
   public orderBook;
+  public chartOptions;
+  public chartData;
 
   constructor(
     private orderBookService: OrderBookService,
   ) { }
 
   public ngOnInit() {
+    this.chartOptions = {
+      chart: {
+        type: 'stackedAreaChart',
+        title: 'Asks',
+        height: 450,
+        // color: '#ffffff',
+        // margin : {
+        //   top: 20,
+        //   right: 20,
+        //   bottom: 30,
+        //   left: 40
+        // },
+        x: function(d) {
+          return d[0];
+        },
+        y: function(d) {
+          return d[1];
+        },
+        useVoronoi: false,
+        clipEdge: true,
+        duration: 100,
+        useInteractiveGuideline: true,
+        xAxis: {
+          showMaxMin: false,
+          tickFormat: function(d) {
+            return d3.format(',.8f')(d);
+          },
+          axisLabel: 'Price'
+        },
+        yAxis: {
+          tickFormat: function(d){
+            return d3.format('1f')(d);
+          },
+          axisLabel: 'Volume'
+        },
+        zoom: {
+          enabled: true,
+          scaleExtent: [1, 10],
+          useFixedDomain: false,
+          useNiceScale: false,
+          horizontalOff: false,
+          verticalOff: true,
+          unzoomEventType: 'dblclick.zoom'
+        }
+      }
+    };
+
     this.orderBookService
       .retrieveOrderBook('BTC-ETH')
       .subscribe((data) => {
         this.orderBook = data;
+        this.chartData = [
+          {
+            key: 'Poloniex',
+            values: this.orderBook.poloniex.asks
+          }, {
+            key: 'Bittrex',
+            values: this.orderBook.bittrex.asks
+          }];
       });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { NvD3Module } from 'ng2-nvd3'
 
 import { AppComponent } from './app.component';
 
 import { ApiService } from './providers/api.service';
 import { OrderBookService } from './providers/order.book.service';
+
+import 'd3';
+import 'nvd3';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,8 @@ import { OrderBookService } from './providers/order.book.service';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    NvD3Module
   ],
   providers: [ApiService, OrderBookService],
   bootstrap: [AppComponent]
