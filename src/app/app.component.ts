1b3267466b7d5cf3a4bb0b8704ef0e9109d71a04
plantsmiles <plantsmiles@gmail.com>
2017-10-08 20:03:03 -0600

    create tables to display combined order books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { OrderBookService } from './providers/order.book.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -7,74 +8,106 @@ import { OrderBookService } from './providers/order.book.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public orderBook;
-  public chartOptions;
-  public chartData;
+  orderBook;
+  bids = [];
+  asks = [];
+  loadingIndicator = true;
+  reorderable = true;
+
+  bidColumns = [
+    { name: 'Bid' },
+    { name: 'Bittrex Volume' },
+    { name: 'Poloniex Volume' },
+    { name: 'Total Volume' },
+    { name: 'Overlap'}
+  ];
+
+  askColumns = [
+    { name: 'Ask' },
+    { name: 'Bittrex Volume' },
+    { name: 'Poloniex Volume' },
+    { name: 'Total Volume' },
+    { name: 'Overlap'}
+  ];
 
   constructor(
     private orderBookService: OrderBookService,
   ) { }
 
   public ngOnInit() {
-    this.chartOptions = {
-      chart: {
-        type: 'stackedAreaChart',
-        title: 'Asks',
-        height: 450,
-        // color: '#ffffff',
-        // margin : {
-        //   top: 20,
-        //   right: 20,
-        //   bottom: 30,
-        //   left: 40
-        // },
-        x: function(d) {
-          return d[0];
-        },
-        y: function(d) {
-          return d[1];
-        },
-        useVoronoi: false,
-        clipEdge: true,
-        duration: 100,
-        useInteractiveGuideline: true,
-        xAxis: {
-          showMaxMin: false,
-          tickFormat: function(d) {
-            return d3.format(',.8f')(d);
-          },
-          axisLabel: 'Price'
-        },
-        yAxis: {
-          tickFormat: function(d){
-            return d3.format('1f')(d);
-          },
-          axisLabel: 'Volume'
-        },
-        zoom: {
-          enabled: true,
-          scaleExtent: [1, 10],
-          useFixedDomain: false,
-          useNiceScale: false,
-          horizontalOff: false,
-          verticalOff: true,
-          unzoomEventType: 'dblclick.zoom'
-        }
-      }
-    };
 
     this.orderBookService
       .retrieveOrderBook('BTC-ETH')
       .subscribe((data) => {
         this.orderBook = data;
-        this.chartData = [
-          {
-            key: 'Poloniex',
-            values: this.orderBook.poloniex.asks
-          }, {
-            key: 'Bittrex',
-            values: this.orderBook.bittrex.asks
-          }];
+        this.combineBooks();
+        this.loadingIndicator = false;
       });
+  }
+
+  public combineBooks() {
+    this.bids = [];
+    const bittrexBidPricePoints = this.orderBook.bittrex.bids.map((bid) => {
+      return bid[0];
+    });
+    const bittrexBidVolumnes = this.orderBook.bittrex.bids.map((bid) => {
+      return bid[1];
+    });
+    const poloniexBidPricePoints = this.orderBook.poloniex.bids.map((bid) => {
+      return bid[0];
+    });
+    const poloniexBidVolumnes = this.orderBook.poloniex.bids.map((bid) => {
+      return bid[1];
+    });
+
+    _.union(bittrexBidPricePoints, poloniexBidPricePoints).forEach((pricePoint) => {
+      const bittrexVolume = bittrexBidPricePoints.indexOf(pricePoint) > -1 ?
+        bittrexBidVolumnes[bittrexBidPricePoints.indexOf(pricePoint)] : 0;
+
+      const poloniexVolume = poloniexBidPricePoints.indexOf(pricePoint) > -1 ?
+        poloniexBidVolumnes[poloniexBidPricePoints.indexOf(pricePoint)] : 0;
+
+      const bidRow = {
+        bid: pricePoint,
+        bittrexVolume: bittrexVolume,
+        poloniexVolume: poloniexVolume,
+        totalVolume: bittrexVolume + poloniexVolume,
+        overlap: bittrexVolume > 0 && poloniexVolume > 0
+      };
+
+      this.bids.push(bidRow);
+    });
+
+    this.asks = [];
+    const bittrexAskPricePoints = this.orderBook.bittrex.asks.map((ask) => {
+      return ask[0];
+    });
+    const bittrexAskVolumnes = this.orderBook.bittrex.asks.map((ask) => {
+      return ask[1];
+    });
+    const poloniexAskPricePoints = this.orderBook.poloniex.asks.map((ask) => {
+      return ask[0];
+    });
+    const poloniexAskVolumnes = this.orderBook.poloniex.asks.map((ask) => {
+      return ask[1];
+    });
+
+    _.union(bittrexAskPricePoints, poloniexAskPricePoints).forEach((pricePoint) => {
+      const bittrexVolume = bittrexAskPricePoints.indexOf(pricePoint) > -1 ?
+        bittrexAskVolumnes[bittrexAskPricePoints.indexOf(pricePoint)] : 0;
+
+      const poloniexVolume = poloniexAskPricePoints.indexOf(pricePoint) > -1 ?
+        poloniexAskVolumnes[poloniexAskPricePoints.indexOf(pricePoint)] : 0;
+
+      const askRow = {
+        ask: pricePoint,
+        bittrexVolume: bittrexVolume,
+        poloniexVolume: poloniexVolume,
+        totalVolume: bittrexVolume + poloniexVolume,
+        overlap: bittrexVolume > 0 && poloniexVolume > 0
+      };
+
+      this.asks.push(askRow);
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { NvD3Module } from 'ng2-nvd3'
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppComponent } from './app.component';
 
 import { ApiService } from './providers/api.service';
 import { OrderBookService } from './providers/order.book.service';
-
-import 'd3';
-import 'nvd3';
 
 @NgModule({
   declarations: [
@@ -18,7 +15,7 @@ import 'nvd3';
   imports: [
     BrowserModule,
     HttpModule,
-    NvD3Module
+    NgxDatatableModule
   ],
   providers: [ApiService, OrderBookService],
   bootstrap: [AppComponent]
