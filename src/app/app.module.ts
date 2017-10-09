cca5eb0a6d37c90d7c387e7df2a8745876e4ba17
plantsmiles <plantsmiles@gmail.com>
2017-10-08 20:22:50 -0600

    ditch data table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppComponent } from './app.component';
 
@@ -14,8 +13,7 @@ import { OrderBookService } from './providers/order.book.service';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    NgxDatatableModule
+    HttpModule
   ],
   providers: [ApiService, OrderBookService],
   bootstrap: [AppComponent]
