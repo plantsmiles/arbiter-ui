4f15a837482a00f4da27dc653a5d2944616aef74
plantsmiles <plantsmiles@gmail.com>
2017-10-09 18:27:47 -0600

    now receiving updates via socket io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
 
 import { AppComponent } from './app.component';
 
 import { ApiService } from './service/api.service';
 import { OrderBookService } from './service/order.book.service';
+import { environment } from '../environments/environment';
+
+const config: SocketIoConfig = { url: environment.apiUrl, options: {} };
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { OrderBookService } from './service/order.book.service';
     BrowserModule,
     HttpModule,
     BrowserAnimationsModule,
-    NgxDatatableModule
+    NgxDatatableModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [ApiService, OrderBookService],
   bootstrap: [AppComponent]
